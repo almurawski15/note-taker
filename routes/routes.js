7094b1f5284f507ab957a67f58cd58ac68a54db4
Alexandra Murawski <almurawski15@gmail.com>
2021-05-14 18:21:46 -0400

    wrote out get and post routes, trying to connect html files

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 module.exports = app => {
 
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+    
+
     //Set up the get/post routes for creating notes
     app.get ("/api/notes", function(req, res) {
         res.json(notes);
@@ -15,5 +20,32 @@ module.exports = app => {
         return console.log("Successfully created new note: "+createNote.title);
     });
 
+    //retrieve and delete notes based on specific id
+    app.get("/api/notes/:id", function(req,res) {
+        res.json(notes[req.params.id]);
+    });
 
+    app.delete("/api/notes/:id", function(req, res) {
+        notes.splice(req.params.id, 1);
+        updateDb();
+        console.log("Note deleted for " + req.params.id);
+    });
+
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(__dirname, "../assets/js/notes.html"));
+    });
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, "../assets/js/index.html"));
+    });
+
+    function updateDb() {
+        fs.writeFile("db/db.json",JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+        
+    }
+
+    }); 
 }
